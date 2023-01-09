decee54527f9997adf89d23c3cdb720872a7739a
piokaf05@gmail.com <piokaf05@gmail.com>
2023-01-09 18:36:31 +0100

    Changed the tiles set from object to array

--- server/map.js
+++ server/map.js
@@ -6,11 +6,14 @@ const TEXTURES_PATH = "textures/";
 
 exports.loadTiles = function() {
     const fileContent = fs.readFileSync(JSON_PATH + "tiles.json");
-    const tiles = JSON.parse(fileContent);
+    const tilesObject = JSON.parse(fileContent);
+    const tilesArray = [];
 
-    for(let tileId in tiles) {
-        const tile = tiles[tileId];
+    for(let tileId in tilesObject) {
+        const tile = tilesObject[tileId];
         tile.path = TEXTURES_PATH + "tiles/" + tile.path;
+        tile.name_id = tileId;
+        tilesArray[tile.id] = tile;
     }
-    return tiles;
+    return tilesArray;
 }
