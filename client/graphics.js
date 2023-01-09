3fc6c9babb8d561471c60600ed5e66011d2d8f7a
piokaf05@gmail.com <piokaf05@gmail.com>
2023-01-09 18:16:00 +0100

    Class ImgAsset added

--- client/graphics.js
+++ client/graphics.js
@@ -1,3 +1,44 @@
+class ImgAsset {
+    constructor(source, width, height, sx, sy, swidth, sheight) {
+        this.source = source;
+
+        this.width = width;
+        this.height = height
+
+        this.sx = sx;
+        this.sy = sy;
+
+        this.swidth = swidth;
+        this.sheight = sheight;
+
+        this.init();
+    }
+    init() {
+        if((typeof this.source) == "string") {
+            this.image = document.createElement("img");
+            this.image.onload = this.ready.bind(this);
+            this.image.src = this.source;
+        }
+        if((typeof this.source) == "object") {
+            this.image = this.source;
+            this.ready();
+        }
+    }
+    ready() {
+        if(this.sx == undefined || this.sy == undefined || !this.swidth || !this.sheight) {
+            this.sx = 0;
+            this.sy = 0;
+
+            this.swidth = this.image.width;
+            this.sheight = this.image.height;
+        }
+    }
+    draw(x, y) {
+        ctx.imageSmoothingEnabled = false;
+        ctx.drawImage(this.image, this.sx, this.sy, this.swidth, this.sheight, x, y, this.width, this.height);
+    }
+}
+
 function loadImage(path) {
     const image = document.createElement("img");
     image.src = path;
