ab0278b3dd727e99407a65a4f0913b38de498728
piokaf05@gmail.com <piokaf05@gmail.com>
2023-01-09 19:34:10 +0100

    Basic preparing of player moving system

--- client/keyboard.js
+++ client/keyboard.js
@@ -1,5 +1,13 @@
 const keys = [];
 
+const LEFT = 0;
+const RIGHT = 1;
+const UP = 2;
+const DOWN = 3;
+
+const movingKeys = [];
+const allTheRightMoves = [];
+
 function initKeyboard() {
     document.body.onkeydown = function(event) {
         keyDown(event);
@@ -7,11 +15,23 @@ function initKeyboard() {
     document.body.onkeyup = function(event) {
         keyUp(event);
     }
+    initMovingSystem();
+}
+function initMovingSystem() {
+    movingKeys[LEFT] = "A";
+    movingKeys[RIGHT] = "D";
+    movingKeys[UP] = "W";
+    movingKeys[DOWN] = "S";
+
+    allTheRightMoves[LEFT] = [-1, 0];
+    allTheRightMoves[RIGHT] = [1, 0];
+    allTheRightMoves[UP] = [0, -1];
+    allTheRightMoves[DOWN] = [0, 1];
 }
 
 function keyDown(event) {
-    keys[event.key] = true;
+    keys[event.key.toUpperCase()] = true;
 }
 function keyUp(event) {
-    keys[event.key] = false;
+    keys[event.key.toUpperCase()] = false;
 }
